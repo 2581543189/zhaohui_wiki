3d81c0560d6c8c6248129399623ac5164a07e552
赵晖 <>
2018-11-05 20:11:40 +0800

    测试cdn加速

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -101,7 +101,7 @@ export default {
   cssnano: {
     mergeRules: false
   },
-  publicPath: "http://47.99.76.20/frontend/",//build 的时候使用
+  publicPath: "http://www.zhaohui.wiki/frontend/",//build 的时候使用
   //publicPath: "http://localhost/frontend/",//build 的时候使用
   //base:'/frontend/',//会被拼到网站根目录后边window.routerBase 没有什么卵用
   runtimePublicPath:true,
